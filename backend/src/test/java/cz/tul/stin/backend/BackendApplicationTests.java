b5423132e034b0328d8c2e859e591b4a67d5c1e9
LadislavJira <ladislav.jira.020@pslib.cz>
2026-04-22 15:10:20 +0200

    Přidání drobností do backendu a příprava na opětovné nasazení na Azure

--- backend/src/test/java/cz/tul/stin/backend/BackendApplicationTests.java
+++ backend/src/test/java/cz/tul/stin/backend/BackendApplicationTests.java
@@ -3,7 +3,11 @@ package cz.tul.stin.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// Opět podstrčíme proměnné
+@SpringBootTest(properties = {
+        "API_USERNAME=testAdmin",
+        "API_PASSWORD=testPassword123"
+})
 class BackendApplicationTests {
 
     @Test
